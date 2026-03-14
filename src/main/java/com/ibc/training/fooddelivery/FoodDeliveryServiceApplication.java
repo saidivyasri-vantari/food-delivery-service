d0564882c0d13f01f0d4379573ca8b3a8ca7746e
Divya <v.saidivyasri@gmail.com>
2026-03-14 07:22:00 +0530

    Inital project

---
+++ src/main/java/com/ibc/training/fooddelivery/FoodDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ibc.training.fooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryServiceApplication.class, args);
+	}
+
+}
